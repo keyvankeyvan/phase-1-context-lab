4951e8059910faf77e219d9e1f695b5e2007a962
Keyvan Alonso <keyvanalonso@gmail.com>
2022-05-05 15:45:15 -0400

    commit tree 9fb0cc403a11014d6478a16dff9e727d503d952f

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
 /* Your Code Here */
 
+let createEmployeeRecord = function (empRecord) {
+    return {
+        firstName: empRecord[0],
+        familyName: empRecord[1],
+        title: empRecord[2],
+        payPerHour: empRecord[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function (arr) {
+    let records = []
+    arr.forEach(element => {records.push(createEmployeeRecord(element))
+    });
+    return records
+}
+
+let createTimeInEvent = function(dateStamp) {
+    let [datedate, hour] = dateStamp.split(" ")
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour),
+        date: datedate
+    })
+    return this
+}
+
+let createTimeOutEvent = function (dateStamp) {
+    let [datedate, hour] = dateStamp.split(" ")
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: datedate
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function (dateLookup) {
+    const intime = this.timeInEvents.find( (element) => element.date === dateLookup)
+    const outtime = this.timeOutEvents.find((element) => element.date === dateLookup)
+
+    return (outtime.hour - intime.hour)/100
+}
+
+let wagesEarnedOnDate = function (date) {
+    let hrs = hoursWorkedOnDate.call(this, date)
+    let employeeRate = this.payPerHour
+    return (hrs * employeeRate)
+}
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +73,13 @@ const allWagesFor = function () {
     return payable
 }
 
+let findEmployeeByFirstName = function (srcArray, firstName) {
+    let record = srcArray.find((element) => element.firstName === firstName)
+    return record
+}
+
+let calculatePayroll = function (srcArray) {
+    return srcArray.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
